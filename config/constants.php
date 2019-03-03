cd5f6ecb256a3390742492dcd1016bc3e1198ce8
Glascode <mael.querre@gmail.com>
2019-03-04 00:54:24 +0100

    Update constants

--- config/constants.php
+++ config/constants.php
@@ -19,3 +19,6 @@ define('USERNAME_MISSING', 'Vous devez fournir un nom d\'utilisateur.');
 define('USERNAME_TOO_SHORT', 'Le nom d\'utilisateur doit contenir au moins 3 caractères.');
 define('USERNAME_TOO_LONG', 'Le nom d\'utilisateur ne doit pas dépasser 50 caractères.');
 define('USERNAME_CONTAINS_DISALLOWED', 'Votre nom d\'utilisateur ne peut pas contenir des caractères spéciaux.');
+
+// Twitter API
+require_once 'access.php';
