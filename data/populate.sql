ee1997784d4ac0bd3cc99dbd40c6580ad7bf2dfe
Glascode <mael.querre@gmail.com>
2019-03-16 18:33:23 +0100

    Add populate database script

---
+++ data/populate.sql
@@ -0,0 +1,7 @@
+INSERT INTO `rss_feeds` (`id`, `src`, `name`, `url`)
+VALUES (1, 'eurosport', 'Eurosport',
+        'https://www.eurosport.co.uk/football/rss.xml'),
+       (3, 'bbc-sport', 'BBC Sport',
+        'http://feeds.bbci.co.uk/sport/football/rss.xml?edition=uk'),
+       (4, 'nyt', 'New York Times Sports',
+        'http://rss.nytimes.com/services/xml/rss/nyt/Soccer.xml');
