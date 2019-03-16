352e27c44a3159e01fb77fedb7462b0f10acaeee
Glascode <mael.querre@gmail.com>
2019-03-16 18:48:39 +0100

    Add README

---
+++ README.md
@@ -0,0 +1,19 @@
+# Sports Monitor
+
+A sports news trends viewing web app.
+
+
+## Getting started
+
+### Prerequisites
+
+It is assumed you already know how to use LAMP environment.
+
+### Installing
+
+Run `php bin/install.php` in the root directory to initialise and populate the database.
+
+## Authors
+
+- **[Romain Garcia](https://github.com/garcia-dev)**
+- Maël Querré
---
+++ bin/install.php
@@ -0,0 +1,19 @@
+<?php
+
+require '../config/credentials.php';
+
+$options = [
+        PDO::ATTR_PERSISTENT => true,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+];
+
+try {
+    $connection = new PDO('mysql:host=' . DB_HOST, DB_USER, DB_PASSWORD, $options);
+    $sql = file_get_contents('../data/init.sql');
+    $sql .= file_get_contents('../data/populate.sql');
+    $connection->exec($sql);
+
+    echo 'Success! Sports Monitor is ready to use.' . "\n";
+} catch (PDOException $error) {
+    echo $sql . $error->getMessage() . "\n";
+}
